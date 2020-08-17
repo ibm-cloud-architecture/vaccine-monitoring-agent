69ad7be8a60a7ad8e3ffe5890a69ed1e12b50796
Bikash Mainali <bikash.mainali@ibm.com>
2020-08-17 12:46:49 -0400

    added topology and ktable to our database

--- src/main/java/ibm/gse/eda/vaccine/coldchainagent/api/TelemetryResource.java
+++ src/main/java/ibm/gse/eda/vaccine/coldchainagent/api/TelemetryResource.java
@@ -2,11 +2,18 @@ package ibm.gse.eda.vaccine.coldchainagent.api;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.state.QueryableStoreTypes;
+import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
+
+import ibm.gse.eda.vaccine.coldchainagent.domain.ContainerTracker;
 import ibm.gse.eda.vaccine.coldchainagent.domain.Telemetry;
 import ibm.gse.eda.vaccine.coldchainagent.domain.TelemetryAssessor;
 import ibm.gse.eda.vaccine.coldchainagent.infrastructure.scoring.ScoringResult;
@@ -21,11 +28,19 @@ public class TelemetryResource {
 
     @Inject
     TelemetryAssessor assessor;
-    
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    public ScoringResult scoreATelemetry(Telemetry t) {
-        return assessor.callAnomalyDetection(t);
-    }
 
+    @Inject
+    KafkaStreams streams;
+    
+    // @POST
+    // @Consumes(MediaType.APPLICATION_JSON)
+    // public ScoringResult scoreATelemetry(Telemetry t) {
+    //     return assessor.callAnomalyDetection(t);
+    // }
+    @GET
+    @Path("/{id}")
+    public ContainerTracker getContainer(@PathParam("id") String id) {
+        ReadOnlyKeyValueStore<String, ContainerTracker> store=  streams.store(TelemetryAssessor.CONTAINER_TABLE, QueryableStoreTypes.keyValueStore());  
+        return store.get(id);
+    }
 }
--- src/main/java/ibm/gse/eda/vaccine/coldchainagent/domain/TelemetryAssessor.java
+++ src/main/java/ibm/gse/eda/vaccine/coldchainagent/domain/TelemetryAssessor.java
@@ -1,10 +1,12 @@
 package ibm.gse.eda.vaccine.coldchainagent.domain;
 
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.ws.rs.Produces;
+import javax.enterprise.inject.Produces;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.reactive.messaging.Acknowledgment;
@@ -17,6 +19,7 @@ import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.logging.Logger;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
@@ -28,6 +31,11 @@ import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.state.KeyValueBytesStoreSupplier;
 import org.apache.kafka.streams.state.Stores;
+import org.apache.kafka.common.utils.Bytes;
+import org.apache.kafka.streams.state.KeyValueStore;
+import org.apache.kafka.streams.state.QueryableStoreTypes;
+import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
+import org.apache.kafka.streams.kstream.Printed;
 
 import io.quarkus.kafka.client.serialization.JsonbSerde;
 
@@ -39,6 +47,8 @@ import ibm.gse.eda.vaccine.coldchainagent.infrastructure.scoring.ScoringResult;
 import ibm.gse.eda.vaccine.coldchainagent.infrastructure.scoring.ScoringService;
 import ibm.gse.eda.vaccine.coldchainagent.infrastructure.scoring.ScoringTelemetry;
 import ibm.gse.eda.vaccine.coldchainagent.infrastructure.scoring.ScoringTelemetryWrapper;
+import io.reactivex.Flowable;
+import io.smallrye.reactive.messaging.kafka.KafkaRecord;
 
 /**
  * A bean consuming telemetry events from the "reefer-telemetry" Kafka topic and
@@ -48,22 +58,25 @@ import ibm.gse.eda.vaccine.coldchainagent.infrastructure.scoring.ScoringTelemetr
  */
 @ApplicationScoped
 public class TelemetryAssessor {
+    public final static String CONTAINER_TABLE = "containerTableInfo";
     protected static Logger LOG = Logger.getLogger(TelemetryAssessor.class);
     @ConfigProperty(name = "temperature.threshold")
     public double temperatureThreshold;
 
     @ConfigProperty(name = "temperature.max.occurence.count", defaultValue = "3")
-    public double maxCount;
+    public int maxCount;
 
     @ConfigProperty(name = "prediction.enabled", defaultValue = "false")
     public boolean predictions_enabled;
 
-    @ConfigProperty(name="mp.messaging.incoming.reefer-telemetry.topic")
+    @ConfigProperty(name = "mp.messaging.incoming.reefer-telemetry.topic")
     public String reeferTelemetry;
 
-    @Inject
-    @RestClient
-    ScoringService scoringService;
+    // @Inject
+    // @RestClient
+    // ScoringService scoringService;
+
+    public String tableName = "containerTable";
 
     public int count;
     private boolean anomalyFound = false;
@@ -71,95 +84,111 @@ public class TelemetryAssessor {
     public TelemetryAssessor() {
     }
 
+    @Outgoing("reefer-telemetry-out")
+    public Flowable<TelemetryEvent> generate() {
+        System.out.println("Here:");
+        return Flowable.interval(5, TimeUnit.SECONDS)
+                .map(tick -> {
+                    double random = new Random().nextDouble();
+                    double result = 70 + (random * (130 - 70));
+                    TelemetryEvent tel = new TelemetryEvent();
+                    tel.containerID= "contId-1234";
+                    tel.payload = new Telemetry();
+                    tel.payload.temperature = result;
+                    System.out.println(tel);
+                    return tel;
+                });
+    }
+
     /**
      * 
      * @param message
      * @return
      */
-    @Incoming("reefer-telemetry")
-    @Outgoing("reefers")
-    @Acknowledgment(Acknowledgment.Strategy.MANUAL)
-    public PublisherBuilder<Message<ReeferEvent>> processTelemetryEvent(Message<TelemetryEvent> message) {
-        // Get the message as String
-        TelemetryEvent telemetryEvent = message.getPayload();
-        LOG.info("Received message: " + telemetryEvent);
-        if (violateTemperatureThresholdOverTime(telemetryEvent)) {
-            
-        }
-        // query ktable
+    // @Incoming("reefer-telemetry")
+    // @Outgoing("reefers")
+    // @Acknowledgment(Acknowledgment.Strategy.MANUAL)
+    // public PublisherBuilder<Message<ReeferEvent>> processTelemetryEvent(Message<TelemetryEvent> message) {
+    //     // Get the message as String
+    //     TelemetryEvent telemetryEvent = message.getPayload();
+    //     LOG.info("Received message: " + telemetryEvent);
+    //     // if (violateTemperatureThresholdOverTime(telemetryEvent)) {
 
-        if (predictions_enabled) {
-            ScoringResult scoringResult= callAnomalyDetection(telemetryEvent.payload);
-            int prediction = (int)scoringResult.getPredictions()[0].values[0][0];
-            LOG.info("This is the prediction: " + prediction);
-            LOG.info("with a probability: " + "[" + scoringResult.getPredictions()[0].values[0][1] + "," + scoringResult.getPredictions()[0].values[0][1] + "]");
-            // Is there anomaly?
-            anomalyFound = ( prediction == 0 );
-        }
-        else {
-            // Mockup the prediction
-            int number = new Random().nextInt(10);
-            if (number > 6) anomalyFound = true;
-        }
+    //     // }
+    //     // query ktable
 
-        if (!anomalyFound){
-            message.ack(); // All processing of this message is done, ack it now
-            return ReactiveStreams.empty();
-        }
-        else{
-            LOG.info("A reefer anomaly has been predicted. Therefore, sending a ReeferAnomaly Event to the appropriate topic");
-            ReeferEvent cae = new ReeferEvent(
-                        telemetryEvent.containerID, 
-                        telemetryEvent.timestamp,
-                        telemetryEvent.payload);
-            LOG.info("Reefer Anomaly Event object sent: " + cae.toString());
+    //     if (predictions_enabled) {
+    //         ScoringResult scoringResult = callAnomalyDetection(telemetryEvent.payload);
+    //         int prediction = (int) scoringResult.getPredictions()[0].values[0][0];
+    //         LOG.info("This is the prediction: " + prediction);
+    //         LOG.info("with a probability: " + "[" + scoringResult.getPredictions()[0].values[0][1] + ","
+    //                 + scoringResult.getPredictions()[0].values[0][1] + "]");
+    //         // Is there anomaly?
+    //         anomalyFound = (prediction == 0);
+    //     } else {
+    //         // Mockup the prediction
+    //         int number = new Random().nextInt(10);
+    //         if (number > 6)
+    //             anomalyFound = true;
+    //     }
 
-            // This message will be sent on, create a new message which acknowledges the incoming message when it is acked
-            return ReactiveStreams.of(Message.of(cae));
-        }
-    }
+    //     if (!anomalyFound) {
+    //         message.ack(); // All processing of this message is done, ack it now
+    //         return ReactiveStreams.empty();
+    //     } else {
+    //         LOG.info(
+    //                 "A reefer anomaly has been predicted. Therefore, sending a ReeferAnomaly Event to the appropriate topic");
+    //         ReeferEvent cae = new ReeferEvent(telemetryEvent.containerID, telemetryEvent.timestamp,
+    //                 telemetryEvent.payload);
+    //         LOG.info("Reefer Anomaly Event object sent: " + cae.toString());
+
+    //         // This message will be sent on, create a new message which acknowledges the
+    //         // incoming message when it is acked
+    //         return ReactiveStreams.of(Message.of(cae));
+    //     }
+    // }
 
     public boolean violateTemperatureThresholdOverTime(TelemetryEvent telemetryEvent) {
         return true;
     }
 
     @Produces
-    public Topology buildTopology(){
+    public Topology buildTopology() {
         StreamsBuilder builder = new StreamsBuilder();
         // 1- steam from kafka
-        JsonbSerde<TelemetryEvent> aggregationSerde = new JsonbSerde<>(TelemetryEvent.class);
-        KeyValueBytesStoreSupplier storeSupplier = Stores.persistentKeyValueStore(
-                "containerTrackerTable");
-        builder.stream(reeferTelemetry, Consumed.with(Serdes.String(), aggregationSerde))
-                                                        // group event with containerId
-                                                        // .groupBy((k, v) -> v.containerID)
-                                                        .map((k , v)-> new KeyValue<String,TelemetryEvent>(v.containerID, v))
-                                                        .groupByKey()
-                                                        // aggrate data for ktable
-                                                        .aggregate( 
-                                                            // initialized
-                                                            () -> new ContainerTracker(),
-                                                            // aggregator
-                                                            (k, v, aggValue) ->
-                                                                aggValue.update(v),
-                                                                Materialized.<String, ContainerTracker> as(storeSupplier)
-                                                                .withKeySerde(Serdes.String())
-                                                                .withValueSerde(new JsonbSerde<>(ContainerTracker.class)) 
-                                                            );
-
+        JsonbSerde<TelemetryEvent> telemetrySerde = new JsonbSerde<>(TelemetryEvent.class);
+        JsonbSerde<ContainerTracker> containerSerde = new JsonbSerde<>(ContainerTracker.class);
+        System.out.println("inside producer topology");
+        KStream<String, TelemetryEvent> telemetryStream = builder.stream("refarcTopic", Consumed.with(Serdes.String(), telemetrySerde))
+                // .groupBy((k,v) -> KeyValue.pair(v.containerID, v), (Grouped.with(Serdes.String(), telemetrySerde));
+                .map((k, v) -> { 
+                    System.out.println(k + " -> " + v);
+                    System.out.println("Temperature: " + v.payload.temperature);
+                    return new KeyValue<String, TelemetryEvent>(v.containerID, v);
+                });
+        KGroupedStream<String, TelemetryEvent> telemetryGroup = telemetryStream.groupByKey(Grouped.with(Serdes.String(), telemetrySerde));
+        KTable<String, ContainerTracker> containerTable = telemetryGroup.aggregate(
+            () -> new ContainerTracker(maxCount,temperatureThreshold), 
+            (k, v, aggValue) -> aggValue.update(v),
+            Materialized.<String, ContainerTracker, KeyValueStore<Bytes, byte[]>>as(CONTAINER_TABLE)
+            .withKeySerde(Serdes.String())
+            .withValueSerde(containerSerde)     
+        );
+        // containerTable.toStream().print(Printed.<String, ContainerTracker>toSysOut());
+        
         return builder.build();
     }
 
-    public ScoringResult callAnomalyDetection(Telemetry telemetry) {
-        // todo compute last temperature diff
-        ScoringTelemetry st = ScoringTelemetry.build(telemetry,0);
-        ScoringTelemetryWrapper wrapper = new ScoringTelemetryWrapper(st);
-        return scoringService.assessTelemetry(wrapper);
-    }
+    // public ScoringResult callAnomalyDetection(Telemetry telemetry) {
+    //     // todo compute last temperature diff
+    //     ScoringTelemetry st = ScoringTelemetry.build(telemetry, 0);
+    //     ScoringTelemetryWrapper wrapper = new ScoringTelemetryWrapper(st);
+    //     return scoringService.assessTelemetry(wrapper);
+    // }
 
     public double getTemperatureThreshold() {
-		return temperatureThreshold;
-	}
+        return temperatureThreshold;
+    }
 
     public double getMaxCount() {
         return maxCount;
